86e2cb7713080bcf18e5c65dcb8d7199de949c15
yoojin <gina0225@naver.com>
2020-08-02 00:11:47 +0900

    ujin

--- koala/koala/urls.py
+++ koala/koala/urls.py
@@ -1,4 +1,4 @@
-"""mykoala URL Configuration
+"""koala URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from koala_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main, name='main'),
 ]
