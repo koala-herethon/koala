317d849980109f2102d0f6172e5ebca9c96ffd8b
Yewon <kyw20160785@gmail.com>
2020-08-01 22:38:49 +0900

    little...

--- koala_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class KoalaAppConfig(AppConfig):
-    name = 'koala_app'
---
+++ koala_prj/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KoalaPrjConfig(AppConfig):
+    name = 'koala_prj'
